8dac82d497826d641b61044d7f9145e0f66a935e
Elmer Balbin Jr <contact@elmer.dev>
2023-09-20 16:50:46 -0700

    added bonus

---
+++ bonus.js
@@ -0,0 +1,5 @@
+/**
+ * Create a countdown timer from 100 to 0 but instead of going 100,99,98,... subtract by 10 so that it is 100,90,80,70...
+ * Print each number
+ * If it hits 0, print "FINISH" instead of 0
+ */
